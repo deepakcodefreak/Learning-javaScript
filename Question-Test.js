d5afb819e9ea86632c2c03e1732d2887fd547f85
deepakcodefreak <deepak19.engg@gmail.com>
2018-10-24 13:54:37 +0530

    expert coding challenge done

--- Question-Test.js
+++ Question-Test.js
@@ -28,17 +28,22 @@
   var enggQues = new genrateQuestion('what is your branch',['CSE','IT','MECH','ECE',],0)
   var js = new genrateQuestion('Js is Love ',['YES','No'],0);
   var backend = new genrateQuestion('Which is the best server side Technology ?',['PHP','DJANGO','NODE JS','RUBY ON RAILS'],2);
-
   var questions = [enggQues,js,backend];
 
-  //enggQues.printQuestion();
-  //var answer = prompt('enter you option')
-
-
-
+  function random(questions){
     var random = Math.floor(Math.random()*3);
     console.log(questions[random].printQuestion());
     let answer = parseInt(prompt('Enter your result')-1);
     questions[random].checkAnswer(answer);
+    random();
+}
+
+random();    
+
+
+
+
+
+
 
 })();
