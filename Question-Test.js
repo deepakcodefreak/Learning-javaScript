50b550b6d498c7dadf30b567787dcae9f86f4e01
deepakcodefreak <deepak19.engg@gmail.com>
2018-10-24 11:54:18 +0530

    some changes made based upon solution provided in the course

--- Question-Test.js
+++ Question-Test.js
@@ -1,25 +1,44 @@
-function genrateQuestion(ques,options,answer){
-  this.ques = ques;
-  this.options = options;
-  this.answer = answer;
-  this.printQuestion = function(){
-    console.log(this.ques);
-    for (let i = 0; i < options.length; i++) {
-      console.log(`${i+1} ${options[i]}`);
-    }
+
+(function(){
+
+  function genrateQuestion(ques,options,answer){
+    this.ques = ques;
+    this.options = options;
+    this.answer = answer;
+
   }
-}
 
-var enggQues = new genrateQuestion('what is your branch',['CSE','IT','MECH','ECE',],0)
-var js = new genrateQuestion('Js is Love ',['YES','No'],0);
-var backend = new genrateQuestion('Which is the best server side Technology ?',['PHP','DJANGO','NODE JS','RUBY ON RAILS'],2);
+  genrateQuestion.prototype.printQuestion = function() {
+    console.log(this.ques);
+    for (let i = 0; i < this.options.length; i++)
+    {
+      console.log(`${i+1} ${this.options[i]}`);
+    }
+  };
 
-var questions = [enggQues,js,backend];
+  genrateQuestion.prototype.checkAnswer = function(ans) {
+    if (ans === this.answer) {
+      console.log('Correct answer');
+    }else{
+      console.log('Wrong answer');
+    }
 
-//enggQues.printQuestion();
-//var answer = prompt('enter you option')
+  };
 
-function generateRandom(){
-  var random = Math.random()*4+1;
-  console.log(random);
-}
+  var enggQues = new genrateQuestion('what is your branch',['CSE','IT','MECH','ECE',],0)
+  var js = new genrateQuestion('Js is Love ',['YES','No'],0);
+  var backend = new genrateQuestion('Which is the best server side Technology ?',['PHP','DJANGO','NODE JS','RUBY ON RAILS'],2);
+
+  var questions = [enggQues,js,backend];
+
+  //enggQues.printQuestion();
+  //var answer = prompt('enter you option')
+
+
+
+    var random = Math.floor(Math.random()*3);
+    console.log(questions[random].printQuestion());
+    let answer = parseInt(prompt('Enter your result')-1);
+    questions[random].checkAnswer(answer);
+
+})();
