0f509292ab8406b5b34bef4fb6e023fef7be6d85
deepakcodefreak <deepak19.engg@gmail.com>
2018-10-28 21:27:25 +0530

    New way to create private scope in ES6

---
+++ ES6/Blocks&IIFE.js
@@ -0,0 +1,21 @@
+// ES 6
+
+{
+    let a = 10;
+    const b =20;
+    //var c = 30;
+
+}
+
+//console.log(c);
+//console.log(a+b);
+
+
+// ES 5
+
+
+(function(){
+  var  c = 40;
+})
+
+console.log(c);
