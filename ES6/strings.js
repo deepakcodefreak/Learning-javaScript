8da65ec18ef0c40203aab4646097a7f4c562d9ec
deepakcodefreak <deepak19.engg@gmail.com>
2018-10-28 21:28:05 +0530

    New features in ES6 strings

---
+++ ES6/strings.js
@@ -0,0 +1,9 @@
+
+
+let firstName = 'Deepak';
+let lastName = 'Chawla';
+const fullName = `${firstName} ${lastName}`
+
+console.log(firstName.startsWith('D'));
+console.log(lastName.endsWith('la'));
+console.log(fullName.includes(' '));
